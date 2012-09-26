b38bc85476c542684a4cfcbc30444d9eba79dfef
chibinowa <apri@po.wind.ne.jp>
2012-09-27 01:52:04 +0900

    new

---
+++ plugin.customErrorPage.php
@@ -0,0 +1,20 @@
+/**
+ * customErrorPage
+ *
+ * <strong>1.0</strong> ドキュメント毎にエラーページをカスタマイズします
+ * @internal @events OnPageNotFound
+ * プラグイン設定: &ids=<strong>特定のドキュメントに設定する場合</strong><hr>対象にするドキュメントのID;text; &error_id=エラーページにするドキュメントのID<br>※非公開またはウェブユーザ限定のリソースは指定できません。;text; &tvname=<strong>ドキュメント毎に設定する場合</strong><hr>設定に使用するテンプレート変数の名前;text;
+ */
+
+$e = &$modx->Event;
+switch($e->name) {
+  case "OnPageNotFound":
+    if ($tvname && $modx->documentObject[$tvname][1]) {
+      if ($modx->getDocument($modx->documentObject[$tvname][1],'id')) {
+        $modx->config['error_page'] = $modx->documentObject[$tvname][1];
+      }
+    } else if ($ids && $error_id && in_array($modx->documentIdentifier, explode(',',$ids))) {
+      $modx->config['error_page'] = $error_id;
+    }
+  break;
+}
