1f3511d2c82ce8fb7cc46693bfd686d7d159008d
yama <yamamoto@kyms.ne.jp>
2012-09-27 09:33:07 +0900

    コード整理
    (switch文を用いる場合はdefault:return;が必要)

--- plugin.customErrorPage.php
+++ plugin.customErrorPage.php
@@ -6,15 +6,12 @@
  * @internal @properties &ids=<strong>特定のドキュメントに設定する場合</strong><hr>対象にするドキュメントのID;text; &error_id=エラーページにするドキュメントのID<br>※非公開またはウェブユーザ限定のリソースは指定できません。;text; &tvname=<strong>ドキュメント毎に設定する場合</strong><hr>設定に使用するテンプレート変数の名前;text;
  */
 
-$e = &$modx->event;
-switch($e->name) {
-  case "OnPageNotFound":
-    if ($tvname && $modx->documentObject[$tvname][1]) {
-      if ($modx->getDocument($modx->documentObject[$tvname][1],'id')) {
-        $modx->config['error_page'] = $modx->documentObject[$tvname][1];
-      }
-    } else if ($ids && $error_id && in_array($modx->documentIdentifier, explode(',',$ids))) {
-      $modx->config['error_page'] = $error_id;
-    }
-  break;
+if($modx->event->name !== 'OnPageNotFound') return;
+
+if ($tvname && $modx->documentObject[$tvname][1]) {
+  if ($modx->getDocument($modx->documentObject[$tvname][1],'id')) {
+    $modx->config['error_page'] = $modx->documentObject[$tvname][1];
+  }
+} else if ($ids && $error_id && in_array($modx->documentIdentifier, explode(',',$ids))) {
+  $modx->config['error_page'] = $error_id;
 }
